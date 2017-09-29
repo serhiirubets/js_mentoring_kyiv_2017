2fab72cf2a8309ae053fc6627c793ed8e981bad6
Serhii Rubets <serhii_rubets@epam.com>
2017-09-29 18:25:21 +0300

    Start make the second home task

---
+++ src/js/game/Observer.js
@@ -0,0 +1,34 @@
+export default class Observer {
+  constructor() {
+    this.observers = {};
+  }
+
+  subscribe(type, listener) {
+    if (typeof listener !== 'function') {
+      throw new Error('Expected listener to be a function.');
+    }
+
+    if (!this.observers[type]) {
+      this.observers[type] = [];
+    }
+
+    this.observers[type].push(listener);
+
+    // Unsubscribe function
+    return () => {
+      this.observers[type] = this.observers[type].filter(cb => cb !== listener);
+    };
+  }
+
+  dispatch(type, data) {
+    const observers = this.observers[type];
+
+    if (!observers) {
+      throw new Error(`Observer with ${type} does not exist`);
+    }
+
+    observers.forEach((callback) => {
+      callback(data);
+    });
+  }
+}
---
+++ src/js/game/index.js
@@ -0,0 +1,4 @@
+// We have bookmaker’s companies with a lot of kinds of sport and games. There are players who can
+// make a bet for the special game and follow the result. When this game is finished, each player
+// who was made a bet to this game will receive notification about his gain or loss
+
