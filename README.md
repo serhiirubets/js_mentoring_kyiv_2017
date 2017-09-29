88fbeba145b090a54396478065e7a7971edab8f3
Serhii Rubets <serhii_rubets@epam.com>
2017-09-29 13:21:39 +0300

    Init

---
+++ README.md
@@ -0,0 +1,6 @@
+# JavaScript Mentoring Program 2017
+
+## Ho to start:
+* Clone this repo
+* npm i
+* open browser http://localhost:8080
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+    entry: "./src/js/main.js",
+    output: {
+        path: __dirname,
+        filename: "bundle.js"
+    },
+    module: {
+        loaders: [
+            { test: /\.css$/, loader: "style!css" }
+        ],
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['env']
+                    }
+                }
+            }
+        ]
+    }
+};
