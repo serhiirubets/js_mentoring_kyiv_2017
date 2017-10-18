ead6bbab6bb547250cb495b31493dee271192a08
Serhii Rubets <serhii_rubets@epam.com>
2017-10-18 14:09:32 +0300

    Extra it blocks were removed

--- src/js/simple.test.js
+++ src/js/simple.test.js
@@ -5,61 +5,36 @@ describe('Calculator', () => {
   describe('Operations', () => {
     describe('Summary operation', () => {
       it('should return sum of two numbers', () => {
-        const expectedResult = 5;
-        expect(calculator('1 + 4')).to.equal(expectedResult);
-      });
-
-      it('should return sum of two numbers', () => {
-        const expectedResult = 15;
-        expect(calculator('12 + 3')).to.equal(expectedResult);
+        expect(calculator('1 + 4')).to.equal(5);
+        expect(calculator('12 + 3')).to.equal(15);
       });
     });
 
     describe('Difference operation', () => {
       it('should return difference of two numbers', () => {
-        const expectedResult = 10;
-        expect(calculator('20 - 10')).to.equal(expectedResult);
-      });
-
-      it('should return difference of two numbers', () => {
-        const expectedResult = 15;
-        expect(calculator('17 - 2')).to.equal(expectedResult);
+        expect(calculator('20 - 10')).to.equal(10);
+        expect(calculator('17 - 2')).to.equal(15);
       });
     });
 
     describe('Divide operation', () => {
       it('should return result of divide two numbers', () => {
-        const expectedResult = 5;
-        expect(calculator('15 / 3')).to.equal(expectedResult);
-      });
-
-      it('should return result of divide two numbers', () => {
-        const expectedResult = 3;
-        expect(calculator('9 / 3')).to.equal(expectedResult);
+        expect(calculator('15 / 3')).to.equal(5);
+        expect(calculator('9 / 3')).to.equal(3);
       });
     });
 
     describe('Multiplication operation', () => {
       it('should return result of multiply two numbers', () => {
-        const expectedResult = 6;
-        expect(calculator('2 * 3')).to.equal(expectedResult);
-      });
-
-      it('should return result of multiply two numbers', () => {
-        const expectedResult = 15;
-        expect(calculator('5 * 3')).to.equal(expectedResult);
+        expect(calculator('2 * 3')).to.equal(6);
+        expect(calculator('5 * 3')).to.equal(15);
       });
     });
 
     describe('Exponent operation', () => {
       it('should make and return exponent from 2 ** 3 = 8', () => {
-        const expectedResult = 8;
-        expect(calculator('2 ** 3')).to.equal(expectedResult);
-      });
-
-      it('should make and return exponent from 3 ** 3 = 8', () => {
-        const expectedResult = 27;
-        expect(calculator('3 ** 3')).to.equal(expectedResult);
+        expect(calculator('2 ** 3')).to.equal(8);
+        expect(calculator('3 ** 3')).to.equal(27);
       });
     });
   });
