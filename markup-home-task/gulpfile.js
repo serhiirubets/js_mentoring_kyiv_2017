9de0d3f4480253fdec6cdc1e9debe8a76b5f31cb
sergey <sramator@gmail.com>
2017-11-08 21:22:48 +0200

    Html hometask was started

---
+++ markup-home-task/gulpfile.js
@@ -0,0 +1,34 @@
+const gulp = require('gulp');
+const browserSync = require('browser-sync').create();
+const pug = require('gulp-pug');
+const sass = require('gulp-sass');
+
+// Compile pug into html
+gulp.task('views', function() {
+  return gulp.src('app/pug/index.pug')
+    .pipe(pug({}))
+    .pipe(gulp.dest("."))
+});
+
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('sass', function () {
+  return gulp.src("app/styles/*.scss")
+    .pipe(sass())
+    .pipe(gulp.dest("."))
+    .pipe(browserSync.stream());
+});
+
+// Static Server + watching scss/html files
+gulp.task('serve', ['views', 'sass'], function () {
+  browserSync.init({
+    server: "."
+  });
+
+  gulp.watch("app/pug/*.pug", ['views']);
+  gulp.watch("app/styles/*.scss", ['sass']);
+  gulp.watch("index.html").on('change', browserSync.reload);
+});
+
+
+
+gulp.task('default', ['serve']);
