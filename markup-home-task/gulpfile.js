4c4c6158427ef714a44e48dfa0d510ef7747b11b
Serhii Rubets <serhii_rubets@epam.com>
2017-11-13 13:50:38 +0200

    End of the markup

--- markup-home-task/gulpfile.js
+++ markup-home-task/gulpfile.js
@@ -2,33 +2,36 @@ const gulp = require('gulp');
 const browserSync = require('browser-sync').create();
 const pug = require('gulp-pug');
 const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
 
 // Compile pug into html
-gulp.task('views', function() {
+gulp.task('views', () => {
   return gulp.src('app/pug/index.pug')
     .pipe(pug({}))
     .pipe(gulp.dest('.'));
 });
 
 // Compile sass into CSS & auto-inject into browsers
-gulp.task('sass', function () {
-  return gulp.src("app/styles/index.scss")
+gulp.task('sass', () => {
+  return gulp.src('app/styles/index.scss')
     .pipe(sass())
-    .pipe(gulp.dest("."))
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false,
+    }))
+    .pipe(gulp.dest('.'))
     .pipe(browserSync.stream());
 });
 
 // Static Server + watching scss/html files
-gulp.task('serve', ['views', 'sass'], function () {
+gulp.task('serve', ['views', 'sass'], () => {
   browserSync.init({
-    server: "."
+    server: '.',
   });
 
-  gulp.watch("app/pug/*.pug", ['views']);
-  gulp.watch("app/styles/*.scss", ['sass']);
-  gulp.watch("index.html").on('change', browserSync.reload);
+  gulp.watch('app/pug/*.pug', ['views']);
+  gulp.watch('app/styles/*.scss', ['sass']);
+  gulp.watch('index.html').on('change', browserSync.reload);
 });
-
-
 
 gulp.task('default', ['serve']);
