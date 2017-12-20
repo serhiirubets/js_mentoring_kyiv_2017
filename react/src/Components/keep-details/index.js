3d141617f11ac44a45bd3cc9720628b9412f5361
sergey <sramator@gmail.com>
2017-12-20 21:58:21 +0200

    Form was added

--- react/src/Components/keep-details/index.js
+++ react/src/Components/keep-details/index.js
@@ -3,6 +3,8 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { getKeepById } from '../../actions';
 
+import './styles.css';
+
 function mapStateToProps(state) {
   return {
     keep: state.keep,
@@ -20,27 +22,95 @@ function mapDispatchToProps(dispatch) {
 
 @connect(mapStateToProps, mapDispatchToProps)
 export default class KeepDetails extends Component {
+  state = {
+    text: '',
+    title: '',
+    tags: []
+  };
+
   static propTypes = {
     keep: PropTypes.shape({
-      title: PropTypes.string.isRequired,
+      title: PropTypes.string,
     }).isRequired,
     match: PropTypes.shape({
-      params: PropTypes.string.isRequired,
+      params: PropTypes.shape({
+        id: PropTypes.string,
+      }),
     }).isRequired,
     getKeepById: PropTypes.func.isRequired,
   };
 
-  componentDidMount() {
+  componentWillMount() {
     const { id } = this.props.match.params;
     this.props.getKeepById(id);
   }
 
+  componentWillReceiveProps(nextProps) {
+    const { title = '', text = '', tags = [] } = nextProps.keep ? nextProps.keep : null;
+    this.setState({
+      text,
+      title,
+      tags
+    })
+
+  }
+
+  onSubmit = (e) => {
+    e.preventDefault();
+    console.log(this.state);
+  };
+
+  onInput = (property, value) => {
+    this.setState({
+      [property]: value,
+    })
+  };
+
   render() {
-    const { title } = this.props.keep;
+    const { title = '', text = '', tags = [] } = this.state;
     return (
-      <article>
-        <h1>{title}</h1>
-      </article>
+      <form className="form" onSubmit={this.onSubmit}>
+        <div className="form-group">
+          <label htmlFor="title-field">Title</label>
+          <input
+            type="text"
+            id="title-field"
+            className="form-control"
+            placeholder="Enter title"
+            value={title}
+            onInput={(e) => this.onInput('title', e.target.value)}
+          />
+          <small className="form-text text-muted">This is awesome title</small>
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="text-field">Text</label>
+          <input
+            type="text"
+            className="form-control"
+            id="text-field"
+            placeholder="Enter text"
+            value={text}
+            onInput={(e) => this.onInput('text', e.target.value.split(' '))}
+          />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="tag-field">Tags</label>
+          <input
+            type="text"
+            className="form-control"
+            id="tag-field"
+            placeholder="Enter tags"
+            value={tags.join(' ')}
+            onInput={(e) => this.onInput('tags', e.target.value.split(' '))}
+          />
+          <small className="form-text text-muted">Введите теги через пробел</small>
+        </div>
+
+        <button type="submit" className="btn btn-primary">Submit</button>
+      </form>
     );
   }
 }
+
